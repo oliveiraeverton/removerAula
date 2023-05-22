241eff30671233e06cfb9cdfa9704c0516c73cb9
Everton de Oliveira <everton.oliveira@ufms.br>
2023-05-22 14:04:05 -0400

    comentario: adicionando comentários no código para ficar mais legível de entender o código

--- ler.cpp
+++ ler.cpp
@@ -4,8 +4,10 @@ int main(){
 
     int ler;
 
+    //comando para ler dados digitados no terminal
     scanf("%d", &ler);
 
+    //comando para imprimir na tela o que foi digitado
     printf("Teste: %d", ler);
 
 
